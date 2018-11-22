d73ff98b0e48a4514c18c74be457377629388255
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-11-22 12:44:16 +0200

    Defining model in Razor page direct.

--- RazorPagesApp/Pages/Factorial.cshtml.cs
+++ RazorPagesApp/Pages/Factorial.cshtml.cs
@@ -1,38 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPagesApp.Pages
-{
-    public class FactorialModel : PageModel
-    {
-        public string Message { get; set; }
+//namespace RazorPagesApp.Pages
+//{
+//    public class FactorialModel : PageModel
+//    {
+//        public FactorialModel()
+//        {
+//        }
 
-        public void OnGet()
-        {
-            Message = "Input a number";
-        }
-
-        public void OnPost(int? number)
-        {
-            if (number == null || number < 1)
-            {
-                Message = "Invalid number. Repeat inputing";
-            }
-            else
-            {
-                int result = 1;
-
-                for (int i = 1; i <= number; i++)
-                {
-                    result *= i;
-                }
-
-                Message = $"Factorial of number {number} is {result}";
-            }
-        }
-    }
-}
+//        public void OnGet()
+//        {
+//        }
+//    }
+//}
